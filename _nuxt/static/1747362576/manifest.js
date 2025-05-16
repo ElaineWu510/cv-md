e1c254d1d8719a670b90c2f2c990f85238b904b3
ElaineWu510 <ElaineWu510@users.noreply.github.com>
2025-05-16 02:29:38 +0000

    deploy: 4dd3866fd0041fbd61c7aa30677ea0cabd2cea74

---
+++ _nuxt/static/1747362576/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fintro","\u002FCV","\u002Fabout","\u002FPublications\u002Fjournal","\u002FPublications\u002Fconference","\u002FPublications\u002FThesis","\u002FTeaching\u002F2021\u002Ffall","\u002FTeaching\u002F2022\u002Fspring"]})
